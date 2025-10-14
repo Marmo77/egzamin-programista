b4f4a586475b819a1a6ab60fed939d12ecb1148a
Marmo77 <matyskamela@gmail.com>
2025-10-14 16:21:11 +0200

    React.memo

--- src/components/Theory/ProgressNavigation.tsx
+++ src/components/Theory/ProgressNavigation.tsx
@@ -13,34 +13,37 @@ import {
   AlertDialogTrigger,
 } from "../ui/alert-dialog";
 import { useNavigate } from "react-router-dom";
+import { memo } from "react";
 
-const ProgressNavigation = ({
-  currentQuestion,
-  question,
-  exam_type,
-}: {
-  currentQuestion: number;
-  question: any[];
-  exam_type: string;
-}) => {
-  return (
-    <div className="max-w-5xl mx-auto max-lg:px-4 max-md:px-2 py-6">
-      <div className="flex flex-col">
-        <div className="flex items-center justify-between mb-2">
-          <span className="text-sm text-muted-foreground">
-            Pytanie {currentQuestion} z {question.length} •{" "}
-            {exam_type.toUpperCase()}
-          </span>
-          <ConfirmExitDialog
-            title="Napewno chcesz wyjść z tego egzaminu?"
-            description="Twoje odpowiedzi nie zostaną zapisane."
-          />
+const ProgressNavigation = memo(
+  ({
+    currentQuestion,
+    question,
+    exam_type,
+  }: {
+    currentQuestion: number;
+    question: any[];
+    exam_type: string;
+  }) => {
+    return (
+      <div className="max-w-5xl mx-auto max-lg:px-4 max-md:px-2 py-6">
+        <div className="flex flex-col">
+          <div className="flex items-center justify-between mb-2">
+            <span className="text-sm text-muted-foreground">
+              Pytanie {currentQuestion} z {question.length} •{" "}
+              {exam_type.toUpperCase()}
+            </span>
+            <ConfirmExitDialog
+              title="Napewno chcesz wyjść z tego egzaminu?"
+              description="Twoje odpowiedzi nie zostaną zapisane."
+            />
+          </div>
+          <Progress value={(currentQuestion / question.length) * 100} />
         </div>
-        <Progress value={(currentQuestion / question.length) * 100} />
       </div>
-    </div>
-  );
-};
+    );
+  }
+);
 
 const ConfirmExitDialog = ({
   title,
