02dc39013c4af64c11342176739de371bddb88ba
Marmo77 <matyskamela@gmail.com>
2025-11-18 18:13:43 +0100

    speedinsights Vercel

--- src/main.tsx
+++ src/main.tsx
@@ -5,11 +5,13 @@ import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
 import ErrorBoundary from "./components/ErrorBoundary";
 import { Toaster } from "sonner";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ErrorBoundary>
       <BrowserRouter>
+        <SpeedInsights />
         <Toaster duration={3000} position="top-right" richColors={true} />{" "}
         {/*Toaster: https://sonner.emilkowal.ski/ */}
         <App />
