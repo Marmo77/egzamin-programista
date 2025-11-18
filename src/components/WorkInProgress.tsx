785732da980d3dfec7b6fe324e7139f2847e680d
Marmo77 <matyskamela@gmail.com>
2025-11-18 17:32:14 +0100

    npm build finished

--- src/components/WorkInProgress.tsx
+++ src/components/WorkInProgress.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { Button } from "./ui/button";
 import { ArrowLeft, Clock, Construction } from "lucide-react";
 import { Card, CardContent, CardHeader } from "./ui/card";
